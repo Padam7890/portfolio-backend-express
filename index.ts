d0c782fab970ae17c617d1dfb2bcc8263f2799db
Padam7890 <padamthapamoho@gmail.com>
2024-07-09 17:01:42 +0545

    new changes

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://padamthapa.com.np/",
+  origin: "https://padamthapa.com.np",
   credentials: true,
 }));
 

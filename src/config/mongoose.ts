61ab6d80c757cc0fc0fe0b0c05cc8e80710410f3
Padam7890 <padamthapamoho@gmail.com>
2024-07-09 13:22:34 +0545

    new

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -9,8 +9,8 @@ const connectDB = async () => {
     await mongoose.connect(db);
     console.log('MongoDB connected...');
   } catch (err: any) {
-    console.error(err.message);
-    process.exit(1); // Exit process with failure
+    // console.error(err.message);
+    // process.exit(1); // Exit process with failure
   }
 };
 

e25368d91e4f35ad082a4d3900347863e9d6cc5c
Padam7890 <padamthapamoho@gmail.com>
2024-07-09 16:54:12 +0545

    cors error fixed

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://padamthapa.com.np/",
   credentials: true,
 }));
 

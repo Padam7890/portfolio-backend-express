62f8afb7c40a2cf93997866e48181a96b7ae8e79
Padam7890 <padamthapamoho@gmail.com>
2024-07-19 11:48:36 +0545

    new changes

--- src/controllers/userController/login.controller.ts
+++ src/controllers/userController/login.controller.ts
@@ -15,7 +15,7 @@ const loginUser = asyncHandler ( async (req, res) => {
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
+      return res.status(400).json({ msg: "Invalid password" });
     }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string);
     return res.json({

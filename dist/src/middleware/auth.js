975ee0e8672ee4ee03205ea5e41de9432a3ac68d
Padam7890 <padamthapamoho@gmail.com>
2024-07-19 14:15:45 +0545

    new changes

--- dist/src/middleware/auth.js
+++ dist/src/middleware/auth.js
@@ -16,29 +16,27 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const models_1 = require("../models/models");
 const asyncHandler_1 = __importDefault(require("./asyncHandler"));
 const checkAuth = (0, asyncHandler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NmQzZmM0OWE5M2JlNThlODc3MjNmOCIsImlhdCI6MTcyMTM3NTMzN30.id6u1dzWOwVgVxjIDVzSLBeqbfCbRD-Ck4rkmqn6g1A";
+    console.log('Authorization Header:', req.headers.authorization); // Log header to verify
+    let token = req.headers.authorization;
     if (!token) {
         return res.status(401).json({ message: "Not Logged in" });
     }
-    console.log('JWT Secret:', process.env.JWT_SECRET); // Should output the secret or `undefined` if not set
-    // Ensure token is in the format 'Bearer <token>'
-    // if (token.startsWith('Bearer ')) {
-    //   token = token.split(' ')[1];
-    // } else {
-    //   return res.status(401).json({ message: "Invalid token format" });
-    // }
+    if (token.startsWith('Bearer ')) {
+        token = token.split(' ')[1];
+    }
+    else {
+        return res.status(401).json({ message: "Invalid token format" });
+    }
     try {
-        // Verify the JWT token
-        const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET_KEY);
-        // Fetch the user from the database
-        const currentUser = yield models_1.User.findOne({
-            where: { id: decoded.id },
-        });
+        const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
+        console.log('Decoded Token:', decoded);
+        // Use the decoded id directly as a string
+        const currentUser = yield models_1.User.findOne({ _id: decoded.id });
         if (!currentUser) {
             return res.status(404).json({ message: "User not found" });
         }
+        console.log('Current User:', currentUser);
         req.user = currentUser;
-        console.log(req.user);
         next();
     }
     catch (error) {
